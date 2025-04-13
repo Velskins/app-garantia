9d38e96566f4f3a8d6d3a72f597d2cf5abd9f7d2
Jeff <jeffbenard974@gmail.com>
2025-04-13 09:48:30 +0200

    Modif dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: false, // Désactive complètement le dark mode
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
